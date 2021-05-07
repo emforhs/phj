e600fc37a5a05127a191a48eeaa59e5590d3bcf0
emforhs <arskrar@gmail.com>
2021-05-07 12:33:32 +0900

    intercepter 설정

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,24 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+axios.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    }
+);
+
+axios.interceptors.response.use((response) => {
+        return response.data
+    },
+    function (error) {
+        if (error.response.status === 401) {
+            
+        }
+        return Promise.reject(error);
+    }
+);
+
+Vue.prototype.$axios = axios;
