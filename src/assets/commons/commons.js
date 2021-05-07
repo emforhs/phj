01dc7b26c3324fefc49bfe05d47653d5a8d8d82e
emforhs <arskrar@gmail.com>
2021-05-08 01:06:22 +0900

    이메일 체크 함수 추가

--- src/assets/commons/commons.js
+++ src/assets/commons/commons.js
@@ -9,13 +9,17 @@ export default{
           setTimeout(()=>{
             chart.render();
           },50);
-        }
+        },
         Vue.prototype.$updateChart = function (el, options){
           ApexCharts.exec(el, 'destroy');
           Vue.prototype.$createChart(el, options);
-        }
+        },
         Vue.prototype.$deleteChart = function (el){
           ApexCharts.exec(el, 'destroy');
+        },
+        Vue.prototype.$isEmail = function (val){
+          var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+          return regExp.test(asValue); // 형식에 맞는 경우 true 리턴	
         }
     }
 }
